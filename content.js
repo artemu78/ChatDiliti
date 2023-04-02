33a8875c276e75dd935f2bb4cae0a0af02e3ad95
Artem Reva <artemu78@yandex.ru>
2023-04-02 17:49:07 +0300

    for some reason it hides first post only, could you please fix it?

--- content.js
+++ content.js
@@ -9,13 +9,40 @@ const createShowPostButton = (post) => {
   post.parentElement.insertBefore(button, post);
 };
 
-const hidePosts = () => {
+const hidePost = (post) => {
+  const authorElement = post.querySelector("strong a, h2 a");
+  const author = authorElement ? authorElement.innerText : "Author not found";
+
+  const timeElement = post.querySelector("abbr");
+  const postDate = timeElement ? timeElement.getAttribute("title") : "Date not found";
+
+  console.log('Hidden post:', { author, postDate });
+  post.style.display = 'none';
+  createShowPostButton(post);
+};
+
+const processPosts = () => {
   const posts = document.querySelectorAll("[data-pagelet^='FeedUnit']");
   posts.forEach((post) => {
-    console.log('Hidden post:', post.innerText);
-    post.style.display = 'none';
-    createShowPostButton(post);
+    if (!post.classList.contains('processed')) {
+      hidePost(post);
+      post.classList.add('processed');
+    }
   });
 };
 
-hidePosts();
+const observeFeed = () => {
+  const feedContainer = document.querySelector('[role="feed"]');
+  if (!feedContainer) {
+    setTimeout(observeFeed, 1000);
+    return;
+  }
+
+  const observer = new MutationObserver((mutations) => {
+    processPosts();
+  });
+
+  observer.observe(feedContainer, { childList: true, subtree: true });
+};
+
+observeFeed();
