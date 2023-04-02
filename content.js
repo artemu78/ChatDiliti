ea5eec5097a5404e8edd5599bc354125e9a8d5ff
Artem Reva <artemu78@yandex.ru>
2023-04-02 16:34:13 +0300

    please, make every post hidden by default with button titled 'show post', which can display original post content

--- content.js
+++ content.js
@@ -1,27 +1,20 @@
+const createShowPostButton = (post) => {
+  const button = document.createElement('button');
+  button.innerHTML = 'Show Post';
+  button.className = 'show-post-button';
+  button.onclick = () => {
+    post.style.display = 'block';
+    button.style.display = 'none';
+  };
+  post.parentElement.insertBefore(button, post);
+};
+
 const hidePosts = () => {
-    const posts = document.querySelectorAll("[data-pagelet^='FeedUnit']");
-    posts.forEach((post) => {
-      post.style.display = "none";
-    });
-  };
-  
-  const showPosts = () => {
-    const posts = document.querySelectorAll("[data-pagelet^='FeedUnit']");
-    posts.forEach((post) => {
-      post.style.display = "block";
-    });
-  };
-  
-  let postsHidden = false;
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.toggle) {
-      if (postsHidden) {
-        showPosts();
-      } else {
-        hidePosts();
-      }
-      postsHidden = !postsHidden;
-    }
+  const posts = document.querySelectorAll("[data-pagelet^='FeedUnit']");
+  posts.forEach((post) => {
+    post.style.display = 'none';
+    createShowPostButton(post);
   });
-  
+};
+
+hidePosts();
