c609bd16b5f3af35023c8fd3a2aa264404f44ce9
Artem Reva <artemu78@yandex.ru>
2023-04-07 19:26:15 +0300

    [meatbag code] Fix button style, add post text

--- src/content.ts
+++ src/content.ts
@@ -2,16 +2,35 @@ const createShowPostButton = (post: HTMLElement): void => {
   const authorAndDate =
     post.querySelector('h4')?.parentElement?.parentElement?.parentElement
       ?.innerText || 'Author and date not found';
+  const postMessage =
+    post.querySelector('[data-ad-preview=message]')?.textContent || '';
   const svg = post.querySelector('svg')?.cloneNode(true) as HTMLElement;
   svg.style.flexShrink = '0';
   svg.style.marginRight = '8px';
-  const text = document.createTextNode(authorAndDate);
+
+  // const textDiv = document.createElement('div');
+  // textDiv.textContent = postMessage?.textContent || '';
 
   const button = document.createElement('button');
-  button.appendChild(svg);
-  button.appendChild(text);
+  button.className = 'custom-button';
 
-  button.className = 'show-post-button';
+  const buttonContent = document.createElement('div');
+  buttonContent.className = 'button-content';
+  const textContent = document.createElement('div');
+  textContent.className = 'text-content';
+  const title = document.createElement('span');
+  title.className = 'title';
+  title.textContent = authorAndDate;
+  const message = document.createElement('p');
+  message.className = 'message';
+  message.textContent = postMessage;
+
+  button.appendChild(buttonContent);
+  buttonContent.appendChild(svg);
+  buttonContent.appendChild(textContent);
+  textContent.appendChild(title);
+  textContent.appendChild(message);
+
   button.onclick = () => {
     post.style.display = 'block';
     button.style.display = 'none';
