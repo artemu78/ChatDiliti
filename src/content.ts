9fb8bb0c7acf31dfd1ab921d5d8ca84479a46e70
Artem Reva <artemu78@yandex.ru>
2023-04-10 14:07:41 +0300

    [meatbag code] Fix dates display in hiding buttons

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,33 @@
+const getPostTime = (authorAndDate: HTMLElement): string => {
+  const useElement = authorAndDate.querySelector('use');
+  if (!useElement) {
+    return '';
+  }
+  const href = useElement.getAttribute('xlink:href');
+  if (!href) {
+    return '';
+  }
+  const timeElement = document.querySelector(href);
+  if (!timeElement) {
+    return '';
+  }
+  return timeElement.textContent || '';
+};
+
 const createShowPostButton = (post: HTMLElement): void => {
+  const authorAndDateElement =
+    post.querySelector('h4')?.parentElement?.parentElement?.parentElement;
   const authorAndDate =
-    post.querySelector('h4')?.parentElement?.parentElement?.parentElement
-      ?.innerText || 'Author and date not found';
+    authorAndDateElement?.innerText || 'Author and date not found';
+  const postTime = authorAndDateElement
+    ? getPostTime(authorAndDateElement)
+    : '';
+
   const postMessage =
     post.querySelector('[data-ad-preview=message]')?.textContent || '';
   const svg = post.querySelector('svg')?.cloneNode(true) as HTMLElement;
   svg.style.flexShrink = '0';
   svg.style.marginRight = '8px';
-
-  // const textDiv = document.createElement('div');
-  // textDiv.textContent = postMessage?.textContent || '';
 
   const button = document.createElement('button');
   button.className = 'custom-button';
@@ -20,7 +38,7 @@ const createShowPostButton = (post: HTMLElement): void => {
   textContent.className = 'text-content';
   const title = document.createElement('span');
   title.className = 'title';
-  title.textContent = authorAndDate;
+  title.textContent = `${authorAndDate} / ${postTime}`;
   const message = document.createElement('p');
   message.className = 'message';
   message.textContent = postMessage;
@@ -47,7 +65,6 @@ const processPosts = (feedContainer: HTMLElement): void => {
   const posts = feedContainer.querySelectorAll<HTMLElement>(
     'div.x1lliihq:not(:is([class*=" "]))',
   );
-  console.log(`ChatDiliti processing ${posts.length} posts`);
   posts.forEach((post) => {
     if (!post.classList.contains('processed')) {
       hidePost(post);
